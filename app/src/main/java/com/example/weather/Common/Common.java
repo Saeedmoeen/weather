1aaa30b4559cfb20fba6500ded91ca56242681b5
Saeed <saeedmoeen@yahoo.com>
2019-03-24 17:23:32 +0430

    Initial commit

---
+++ app/src/main/java/com/example/weather/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.weather.Common;
+
+import android.location.Location;
+
+public class Common {
+    public static final String API_KEY = "631fe568995ce1338cf6f3935f405ed4";
+    public static Location current_location = null;
+}
